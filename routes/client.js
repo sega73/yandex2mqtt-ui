a5bbe586d473203f3c80d0b073888731059e86ff
Evgenii Abramov <john.abramov@gmail.com>
2021-03-20 22:29:16 +0300

    Some refactoring

--- routes/client.js
+++ routes/client.js
@@ -8,6 +8,6 @@ module.exports.info = [
         // `BearerStrategy`. It is typically used to indicate scope of the token,
         // and used in access control checks. For illustrative purposes, this
         // example simply returns the scope in the response.
-        res.json({ client_id: req.user.id, name: req.user.name, scope: req.authInfo.scope });
+        res.json({client_id: req.user.id, name: req.user.name, scope: req.authInfo.scope});
     }
 ];
