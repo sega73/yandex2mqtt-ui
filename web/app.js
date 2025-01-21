52255598d3452a72c1eacc49dd199d7aa1ed4000
KobyletskiSK <buffon961@yandex.ru>
2025-01-21 21:52:42 +0700

    Объединил с наработками из дома

--- web/app.js
+++ web/app.js
@@ -10,9 +10,26 @@ app.set('views', './views');
 app.get('/', function (req, res) {
     res.render('index');
   });
-app.get('/vue', function (req, res) {
-    res.render('index_vue');
-  });
+  app.get('/api/get_users', (req, res) => {
+    // res.send('bb!')
+    const auth_h = req.get('Authorization')
+    // console.log(auth_h)
+    auth_d = auth_h.split(' ')
+    if(auth_d.length < 2) {
+      res.json({error:"Invalid auth header"})
+    }
+    else if(auth_d[0] != "Bearer") {
+      res.json({error:"Invalid auth type"})
+    }
+    else if(auth_d[1] != "test-token") {
+      res.json({error:"Not authorized"})
+    }
+    else {
+      res.json([{login:"admin", name:"Administrator", edit:false},
+        {login:"user1", name:"User 1", edit:false},
+        ])
+    }    
+  })
   // app.get('/img', function (req, res) {
   //   console.log(req);
   // });
